42edcd901f3f44a569810ce2fa68bb9e7ee3fc70
DKNguyen13 <nd2004lk13@gmail.com>
2024-11-18 10:30:47 +0700

    JWT 1

---
+++ src/main/java/vn/hcmute/service/UserService.java
@@ -0,0 +1,19 @@
+package vn.hcmute.service;
+
+import org.springframework.stereotype.Service;
+import vn.hcmute.entity.User;
+import vn.hcmute.repository. UserRepository;
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService (UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
